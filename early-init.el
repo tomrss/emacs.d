198af98d6af03bf9374749dcdfcf31d7a3c45fb3
tomrss <tommaso.rossi1@protonmail.com>
2023-12-23 17:47:23 +0100

    fix

--- early-init.el
+++ early-init.el
@@ -26,12 +26,12 @@
 
 ;;;; Move native compilation cache outside init dir
 
-(when (and (fboundp 'native-compile)
-           (fboundp 'startup-redirect-eln-cache))
-  (let ((eln-cache-dir "~/.cache/emacs/eln-cache/"))
-    (make-directory eln-cache-dir t)
-    (startup-redirect-eln-cache
-     (convert-standard-filename eln-cache-dir))))
+;; (when (and (fboundp 'native-compile)
+;;            (fboundp 'startup-redirect-eln-cache))
+;;   (let ((eln-cache-dir "~/.cache/emacs/eln-cache/"))
+;;     (make-directory eln-cache-dir t)
+;;     (startup-redirect-eln-cache
+;;      (convert-standard-filename eln-cache-dir))))
 
 ;;;; Optimize garbage collections
 
